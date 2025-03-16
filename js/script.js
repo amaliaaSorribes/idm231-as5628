e2c4ea3943da8f5500bb7958bb6832dc40618308
amaliaaSorribes <as5628@drexel.edu>
2025-03-16 17:21:55 -0400

    :art: Wrote instructions + updated size of albums and fonts

--- js/script.js
+++ js/script.js
@@ -144,7 +144,7 @@ function createCD(artist, container){
     const itemSpecific = document.getElementById('item'+artist);
     itemSpecific.appendChild(cd);
 
-    container.style.maxWidth = "1300px";
+    container.style.maxWidth = "1200px";
 }
 
 function deleteCD(cdPrev, prevArtist){
@@ -297,12 +297,17 @@ function getZodiac(month, day) {
         <div class="line line1"></div>
         <div class="line line2"></div>
       </div>
-      <ul>
-        <li>Rule 1</li>
-        <li>Rule 2</li>
-        <li>Rule 3</li>
-        <li>Rule 4</li>
-      </ul>
+      <div class="text-inside">
+      <p><strong>To play an album based on a specific date:</strong></p>
+      <ol>
+        <li><strong>Enter a Date:</strong> Use the date input field to select a date. You can type the date directly or use the calendar picker.</li>
+        <li><strong>Submit:</strong> Click the "Submit" button.</li>
+        <li><strong>Listen:</strong> The album associated with the zodiac sign for that date will begin playing automatically and a CD icon appearing below.</li>
+        <li><strong>View Details:</strong> The song details (title, artist, album name, and release date) along with the zodiac sign roulette at the bottom of the page will update.</li>
+        <li><strong>Stop Playback:</strong> Click on the currently playing album to stop the music.</li>
+      </ol>
+      <p><strong>Alternatively, you can explore the albums by clicking on any of them directly.</strong></p>
+      </div>
     `;
     overlay.appendChild(textDiv);
     container.appendChild(overlay);
